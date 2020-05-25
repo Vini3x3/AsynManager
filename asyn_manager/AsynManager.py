4ddf11a6a4d1928611220ce80f53125638642ac0
Vini3x3 <vincent.hanghang@gmail.com>
2020-05-25 14:17:26 +0800

    Update AsynManager.py

--- asyn_manager/AsynManager.py
+++ asyn_manager/AsynManager.py
@@ -3,4 +3,24 @@ import threading
 
 class AsynManager:
     def __init__(self):
-        pass
+        self.planners = None
+        self.executors = []
+        self.semaphore = threading.Semaphore(len(self.executors))
+        self.threads = []
+
+    def call(self, function_name, *args, **kwargs):
+        func = getattr(self.planners, function_name)
+        self.threads.append(threading.Thread(func, *args, **kwargs))
+        self.threads[-1].run()
+        self.threads[-1].join()
+
+
+class Planner:
+    pass
+
+
+class Executor:
+    pass
+
+
+
